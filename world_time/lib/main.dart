d135e0a912239c0026af409cb521df8d878782b1
huzaifa215 <66369515+huzaifa215@users.noreply.github.com>
2020-11-28 14:11:42 +0500

    Font front end font style

--- world_time/lib/main.dart
+++ world_time/lib/main.dart
@@ -7,6 +7,23 @@ void main(){
         backgroundColor: Colors.black12,
         centerTitle: true,
       ),
+      body: Center(
+        child:Text('Enter the Location', style:TextStyle(
+          fontStyle: FontStyle.italic,
+          color: Colors.black87,
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+        )
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+      child:Text('Click'),
+        backgroundColor: Colors.blueGrey,
     ),
-  ));
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat, // for the location
+      ),
+    ),
+  );// run app
 }
