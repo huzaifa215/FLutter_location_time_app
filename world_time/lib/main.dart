44c7907e1d452bd41e3e15acd8b871583856e313
huzaifa215 <66369515+huzaifa215@users.noreply.github.com>
2020-11-29 03:37:56 +0500

    stateless weidget

--- world_time/lib/main.dart
+++ world_time/lib/main.dart
@@ -1,29 +1,39 @@
 import 'package:flutter/material.dart';
-void main(){
+void main() {
   runApp(MaterialApp(
-    home:Scaffold(
-      appBar: AppBar(
-        title: Text("World TIme"),
-        backgroundColor: Colors.black12,
-        centerTitle: true,
-      ),
-      body: Center(
-        child:Text('Enter the Location', style:TextStyle(
-          fontStyle: FontStyle.italic,
-          color: Colors.black87,
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-        )
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-      child:Text('Click'),
-        backgroundColor: Colors.blueGrey,
-    ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat, // for the location
-      ),
-    ),
-  );// run app
+    home: Home()
+  ),
+  ); // run app
 }
+
+  // Run the code automatically the stless wedgit is used
+  class Home extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("World TIme"),
+          backgroundColor: Colors.black12,
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text('Enter the Location', style: TextStyle(
+            fontStyle: FontStyle.italic,
+            color: Colors.black87,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+          )
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text('Click'),
+          backgroundColor: Colors.blueGrey,
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation
+            .centerFloat, // for the location
+      );
+    }
+  }
+
