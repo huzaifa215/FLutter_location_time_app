2e806af55afba80d5eed485ea63fea40e8833fa6
huzaifa215 <66369515+huzaifa215@users.noreply.github.com>
2020-12-24 11:59:43 +0500

    future<void> asyns

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
 import'package:http/http.dart';
 //jason decode
 import 'dart:convert';
@@ -7,31 +8,19 @@ class Loading extends StatefulWidget {
   @override
   _LoadingState createState() => _LoadingState();
 }
-
 class _LoadingState extends State<Loading> {
-  void getTime() async{ // keh jb tk ye nhi ho age proceed nhi karen ge
-    //make the request
-    Response response=await get('http://worldtimeapi.org/api/timezone/Asia/Karachi');
-    Map data=jsonDecode(response.body);
-  //  print(data);
-    // get properties from data
-    String datetime=data['datetime'];
-    String offset=data['utc_offset'].substring(1,3);
-   //  print(datetime);
-   // print(offset);
-    //create an date time obj
-    DateTime now=DateTime.parse(datetime);
-    now=now.add(Duration(hours:int.parse(offset)));
-    print(now);
+
+  void setupWorldTime() async{
+    WorldTime worldTime= WorldTime(location: 'Berlin',flag: 'germany.png',url: 'Europe/Berlin');
+    await worldTime.getTime();
+    print(worldTime.time);
   }
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getTime();
+    setupWorldTime();
   }
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
---
+++ world_time/lib/services/world_time.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+class WorldTime{
+  String location;// location from the UI
+  String url;// the url like asia/pakistan
+  String time;//the time of that location
+  String flag;// the flag of that country
+
+  WorldTime({this.location,this.flag,this.url});// constructor
+  // futur is a keyword that act as apromise or a placeholder taht this fuction will complete perform than tha other come
+  Future<void> getTime() async{ // keh jb tk ye nhi ho age proceed nhi karen ge
+    //make the request
+    Response response=await get('http://worldtimeapi.org/api/timezone/$url');
+    Map data=jsonDecode(response.body);
+    //  print(data);
+    // get properties from data
+    String datetime=data['datetime'];
+    String offset=data['utc_offset'].substring(1,3);
+    //  print(datetime);
+    // print(offset);
+    //create an date time obj
+    DateTime now=DateTime.parse(datetime);
+    now=now.add(Duration(hours:int.parse(offset)));
+    // convert the now to the string
+    time=now.toString();
+  }
+}
+
+
+
